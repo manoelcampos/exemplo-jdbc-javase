798aca6945b35b90dae3bf6b4d15153935861d5e
Manoel Campos <manoelcampos@gmail.com>
2024-09-13 18:13:10 -0300

    Atualiza docs sobre a string de conexão para o H2
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- src/main/java/exemplojdbc/ExemploJdbc.java
+++ src/main/java/exemplojdbc/ExemploJdbc.java
@@ -13,8 +13,12 @@ public class ExemploJdbc {
      * String de conexão com o banco de dados.
      * O banco de dados utilizado é o Apache H2, que está configurado
      * na string de conexão para ser um banco em memória.
+     * Você pode criar o banco em arquivo, alterando a estrutura da String de conexão para
+     * jdbc:h2:file:/caminho/nome_do_banco
+     * Por exemplo: jdbc:h2:file:./banco_exemplo
+     * onde ./ indica o diretório atual do projeto.
      */
-    private final String CONNECTION_URL = "jdbc:h2:mem:meubanco";
+    private final String CONNECTION_URL = "jdbc:h2:mem:banco_exemplo";
     private final String USERNAME = "sa";
     private final String PASSWORD = "password";
 
